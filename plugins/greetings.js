535a8ac47ce8e2f5e6b716dbdadd62fa3578bd11
NishNishendanidu <96036613+NishNishendanidu@users.noreply.github.com>
2022-02-19 22:20:47 +0530

    Update greetings.js

--- plugins/greetings.js
+++ plugins/greetings.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2021 Queen Amdi.
+/* no Copyright (C) 2021  king amda-X
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Queen Amdi - Black Amda
+king amda-X- nishen danidu
 */
 
 const Amdi = require('../events');
